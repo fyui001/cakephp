ab12dc9c9b218f877fbc1dbea00065d1c007be9a
Yui501 <yun.cod@gmail.com>
2018-12-14 19:43:30 +0900

    Merge branch 'photo' of https://github.com/fyui001/cakephp into photo

--- app/Controller/PhotosController.php
+++ app/Controller/PhotosController.php
@@ -13,25 +13,42 @@ class PhotosController extends AppController{
 
   public function add(){
     $this->loadModel('Photos');
+    debug($_FILES);
 
-    $FilePath = "/var/www/html/cakephp/app/Photo/";
-    if(!file_exists($FilePath)){
-      mkdir($FilePath);
+    $FilePath = "/Photo/"; /* バリデーション後保存するディレクトリ */
+    $FilePath_c = "/var/www/html/cakephp/app/tmp/CheckPhoto/"; /* 最初に保存するディレクトリのパス */
+    $FileName = $_FILES['image']['name']; /* ファイル名を変数へ代入 */
+
+    if(!file_exists($FilePath_c)){
+      mkdir($FilePath_c);  /* ディレクトリが存在しない場合は其れを作成 */
     }
 
-    /* ファイル名変更 */
-    $FileName = $_FILES['image']['name'];
-    $UploadPath = "/Photo'{$FileName}'";
+    $UploadPath_c = "{$FilePath_c}{$FileName}";/* パスとファイル名を結合 */
+
+    /* tmpディレクトリにアップロード */
+    if(move_uploaded_file($_FILES['image']['tmp_name'], $UploadPath_c)){
+      if($this->Photos->save(array('path' => $UploadPath_c))){
+        echo "success\n";
+
+        try {
+          $name = "{$FilePath}{$FileName}";
+          $image = new Imagick("/var/www/html/cakephp/app/tmp/CheckPhoto/IMG_0019.JPG");
+          if(ture){
+            $image->writeImage("/var/www/html/cakephp/app/webroot/Photo/IMG_0019.JPG");
+          }else{
+            throw new Exception();
+          }
+        }catch(Exception $e) {
+          echo "にゃーん";
+        }
 
 
-    if(move_uploaded_file($_FILES['image']['tmp_name'], $UploadPath)){
-      if($this->Photos->save(array('path' => $UploadPath))){
-        echo 'success';
-      }
-
+      }else{
+        echo 'あっぷろーどに失敗';
     }
 
   }
+}
 
   public function upload(){
 
--- app/View/Photos/index.ctp
+++ app/View/Photos/index.ctp
@@ -1,3 +1,7 @@
 <?php
   ?>
-  <img src="/Photo/_DSC0827.jpg" alt="">
+  <h1>画像あっぷろーど</h1>
+  <form id="upload" action="/photos/add" method="post" enctype="multipart/form-data">
+    <input type="file" name="image" id = "data">
+    <input type="submit" value="あっぷろーど">
+  </form>
