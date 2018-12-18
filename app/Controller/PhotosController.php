097baa571bbc44d0707d6a4cc014c1bc600709f6
Yui501 <yun.cod@gmail.com>
2018-12-19 02:19:44 +0900

    commit

--- app/Controller/PhotosController.php
+++ app/Controller/PhotosController.php
@@ -11,49 +11,34 @@ class PhotosController extends AppController{
     $path = $this->Photo->find('all', array('fields' => 'path'));
     $path_n = count($path);
     for($i = 0; $i < $path_n; $i++){
-      $p[] = $path[$i]['Photo']['path'];
+      $p[$i] = $path[$i]['Photo']['path'];
     }
     $this->set('photo',$p);
-    header("Content-Type: image/JPG");
-    for($i = 0; $i < $path_n; $i++){
-      $p[] = $path[$i]['Photo']['path'];
-    }
-    $image = new Imagick($p[1]);
-    echo $image;
 
-
-    return $p;
+    //return $p;
   }
 
   public function add(){
-    $this->loadModel('Photos');
+
+    //if($this->request->is('post')){
+    debug($_FILES);
 
     $FileName = $_FILES['image']['name']; /* ファイル名を変数へ代入 */
-    $UploadPath = "/var/www/html/cakephp/app/webroot/Photo/{$FileName}"; /* パスとファイル名を結合 */
-    $ViwePath = "/Photo/{$FileName}"; /* データベースに保存するパス */
+    $UploadPath = "/var/www/html/cakephp/app/webroot/Photo/{$FileName}"; /* バリデーション後保存するディレクトリのパス */
+    $ViewPath = "/Photo/{$FileName}"; /* データベースに保存するパス */
     $UploadPath_c = "/var/www/html/cakephp/app/tmp/CheckPhoto/{$FileName}"; /* 最初に保存するディレクトリのパス (tmpディレクトリなど)*/
 
     /* 最初に保存するディレクトリにアップロード */
     if(move_uploaded_file($_FILES['image']['tmp_name'], $UploadPath_c)){
 
       /*画像を保存しているディレクトリのパスをデータベースへ保存 */
-      if($this->Photo->save(array('path' => $ViwePath))){
-
-    $FileName = $_FILES['image']['name']; /* ファイル名を変数へ代入 */
-    $FilePath = "/var/www/html/cakephp/app/webroot/Photo/"; /* バリデーション後保存するディレクトリのパス */
-    $FilePath_c = "/var/www/html/cakephp/app/tmp/CheckPhoto/"; /* 最初に保存するディレクトリのパス */
-    $UploadPath = "{$FilePath}{$FileName}"; /* パスとファイル名結合 */
-    $UploadPath_c = "{$FilePath_c}{$FileName}";/* パスとファイル名を結合(チェック用) */
-
-    /* tmpディレクトリにアップロード */
-    if(move_uploaded_file($_FILES['image']['tmp_name'], $UploadPath_c)){
-
-      /*画像を保存しているディレクトリのパスをデータベースへ保存 */
       if($this->Photo->save(array('path' => $UploadPath))){
-        echo "success\n";
+
         try {
           if($image = new Imagick($UploadPath_c)){
             $image->writeImage($UploadPath); /* Imagickでアップロードされたファイル画像かどうか確認 */
+            $this->Photo->save(array('path' => $ViewPath)); /* 画像を保存しているディレクトリのパスをデータベースへ保存 */
+            echo 'success';
           }else{
             throw new Exception();
           }
@@ -67,9 +52,6 @@ class PhotosController extends AppController{
 
     }
   }
-
-  public function upload(){
-
-  }
+//}
 
 }
--- app/View/Photos/index.ctp
+++ app/View/Photos/index.ctp
@@ -4,5 +4,6 @@ echo $photo[0];
 $photo_n = count($photo);
 echo $photo_n;
 for ($i=0; $i < $photo_n; $i++) {
-  echo "<img src='{$photo[$i]}'>";
+ echo "<img src='{$photo[$i]}'>";
+
 }
